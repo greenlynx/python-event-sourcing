c7cc174c382063b77ff570ff32e19f9f645f06bc
Dan Haughey <dan@greenlynx.co.uk>
2023-01-24 20:52:29 +0000

    Add aggregate ID

--- events.py
+++ events.py
@@ -20,7 +20,7 @@ def parse_options():
         'add', help='adds a new event to the event store')
     parser_a.add_argument('event_type', choices=[
                           'open_account', 'close_account'])
-    parser_a.add_argument('data')
+    parser_a.add_argument('account_id')
     parser_a.set_defaults(func=do_add)
 
     parser_b = subparsers.add_parser(
@@ -37,19 +37,28 @@ def parse_options():
 
 def build_event(event_type, data):
     return {
-        "event_type": event_type, "data": data
+        "type": event_type, "data": data
     }
 
 
-def build_record(event, state):
+def build_record(aggregate_id, event, state):
     return {
+        "pk": aggregate_id,
         "event": event,
         "state": state
     }
 
 
-def deconstruct_record(record):
+def aggregate_id_from_record(record):
+    return record["pk"]
+
+
+def event_from_record(record):
     return record["event"]
+
+
+def state_from_record(record):
+    return record["state"]
 
 
 def calculate_state(events):
@@ -58,11 +67,13 @@ def calculate_state(events):
 
 def do_add(args):
     records = read_records()
-    existing_events = map(deconstruct_record, records)
+    existing_events = map(event_from_record, records)
 
-    new_event = build_event(args.event_type, args.data)
+    aggregate_id = args.account_id
+    new_event = build_event(args.event_type, {})
     new_state = calculate_state(itertools.chain(existing_events, [new_event]))
-    new_record = build_record(new_event, new_state)
+    new_record = build_record(
+        aggregate_id, new_event, new_state)
 
     records.append(new_record)
     write_records(records)
@@ -74,7 +85,7 @@ def do_list(args):
 
 def do_state(args):
     records = read_records()
-    events = map(deconstruct_record, records)
+    events = map(event_from_record, records)
     current_state = calculate_state(events)
     print(current_state)
 
@@ -97,7 +108,7 @@ def get_initial_state():
 
 
 def apply_event(current_state, event):
-    if (event["event_type"] == "open_account"):
+    if (event["type"] == "open_account"):
         current_state["number_of_accounts"] += 1
     else:
         raise Exception("Unknown event")
